de5f602baad61081fbd014d39fa5feb87f001536
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-01-11 15:29:27 +0100

    push 1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  import AppTitle from './components/AppTitle.vue';
+  export default{
+    components:{
+      AppTitle
+    },
+    data() {
+      return {
+        title: `la mia prima app con vue`
+      }
+    },
+  }
+</script>
+
+<template>
+  <div>
+    <AppTitle></AppTitle>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name:`AppTitle`,
+    data(){
+        return{
+            hello:`La mia prima app con vue`
+        }
+    }   
+}
+</script>
+
+<template>
+    <div>
+        <h1 class="text-red">
+            {{hello}}
+        </h1>
+    </div>
+</template>
+
+<style>
+.text-red{ 
+    color: red;
+}
+</style>
